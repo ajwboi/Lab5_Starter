fcbdd628a79b325ddf2a9ee1c805d4bd217c794b
ajwboi <82994026+ajwboi@users.noreply.github.com>
2022-10-31 19:16:27 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,44 +1,54 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+const jsConfetti = new JSConfetti();
+var horn;
+var img;
 var sound;
+var vol;
 
 function init() {
-  document.querySelector("#horn-select").addEventListener('change', changeHorn);
-  document.querySelector("button").addEventListener('click', playAudio);
+  document.querySelector("#horn-select").addEventListener('change',changeHorn);
   document.getElementById("volume-controls").addEventListener('input', changeVolIcon);
+  document.querySelector("button").addEventListener('click', playAudio);
 }
 
 function changeHorn() {
-  var horn = document.querySelector("#horn-select");
-  var image = document.querySelector("expose img");
+  horn = document.querySelector("#horn-select");
+  img = document.querySelector("#expose img");
+  sound = document.querySelector("audio");
 
   if (horn.value == "air-horn") {
-    sound.src = "./assets/audio/air-horn.mp3";
-    image.src = "./assets/images/air-horn.mp3";
+    sound.src = "/assets/audio/air-horn.mp3";
+    img.src = "/assets/images/air-horn.svg";
   }
   else if (horn.value == "party-horn") {
-    sound.src = "./assets/audio/party-horn.mp3";
-    image.src = "./assets/images/party-horn.mp3";
+    sound.src = "/assets/audio/party-horn.mp3";
+    img.src = "/assets/images/party-horn.svg";
   }
   else if (horn.value == "car-horn") {
-    sound.src = "./assets/audio/car-horn.mp3";
-    image.src = "./assets/images/car-horn.mp3";
+    sound.src = "/assets/audio/car-horn.mp3";
+    img.src = "/assets/images/car-horn.svg";
   }
 
   else {
     sound.src = "";
-    image.src = "no-image.png";
+    img.src = "no-image.png";
   }
 }
 
 function playAudio() {
+  sound = document.querySelector("audio");
+  sound.volume = (document.querySelector("#volume-controls input").value / 100);
   sound.play();
+
+  if (document.querySelector("#horn-select").value == "party-horn") 
+    jsConfetti.addConfetti();
 }
 
 function changeVolIcon() {
-  var vol = document.querySelector("volume-controls input").value;
-  var image = document.querySelector("volume controls img");
+  vol = document.querySelector("#volume-controls input").value;
+  var image = document.querySelector("#volume-controls img");
 
   if (vol == 0) 
     image.src = "./assets/icons/volume-level-0.svg";
